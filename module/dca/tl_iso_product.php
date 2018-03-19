5bca23c19503bb15516c09d939006b8f6d921695
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-03-19 13:11:32 +0100

    Display stock in the product list.

--- module/dca/tl_iso_product.php
+++ module/dca/tl_iso_product.php
@@ -3,17 +3,22 @@
 /**
  * This file is part of richardhj/contao-isotope_simple_stockmanagement.
  *
- * Copyright (c) 2016-2017 Richard Henkenjohann
+ * Copyright (c) 2016-2018 Richard Henkenjohann
  *
  * @package   richardhj/contao-isotope_simple_stockmanagement
  * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright 2016-2017 Richard Henkenjohann
- * @license   https://github.com/richardhj/richardhj/contao-isotope_simple_stockmanagement/blob/master/LICENSE LGPL-3.0
+ * @copyright 2016-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/contao-isotope_simple_stockmanagement/blob/master/LICENSE LGPL-3.0
  */
 
+use Richardhj\Isotope\SimpleStockManagement\BackendIntegration\LabelCallbackListener;
 
 $table = Isotope\Model\Product::getTable();
 
+$GLOBALS['TL_DCA'][$table]['list']['label']['fields'][] = 'stock';
+
+$GLOBALS['TL_DCA'][$table]['list']['label']['label_callback.default'] = $GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'];
+$GLOBALS['TL_DCA'][$table]['list']['label']['label_callback'] = [LabelCallbackListener::class, 'generate'];
 
 /**
  * Fields
---
+++ src/BackendIntegration/LabelCallbackListener.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-isotope_simple_stockmanagement.
+ *
+ * Copyright (c) 2016-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/contao-isotope_simple_stockmanagement
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2016-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/contao-isotope_simple_stockmanagement/blob/master/LICENSE LGPL-3.0
+ */
+
+namespace Richardhj\Isotope\SimpleStockManagement\BackendIntegration;
+
+
+use Contao\System;
+use Isotope\Model\Product;
+use Richardhj\Isotope\SimpleStockManagement\Model\Stock;
+
+class LabelCallbackListener
+{
+    /** @noinspection MoreThanThreeArgumentsInspection
+     *
+     * @param $row
+     * @param $label
+     * @param $dc
+     * @param $args
+     *
+     * @return mixed
+     *
+     * @throws \Exception If type is not a related field of tl_iso_product
+     */
+    public function generate($row, $label, $dc, $args)
+    {
+        list (
+            $callbackClass, $callbackMethod
+            ) = $GLOBALS['TL_DCA']['tl_iso_product']['list']['label']['label_callback.default'];
+        $args = System::importStatic($callbackClass)->{$callbackMethod}($row, $label, $dc, $args);
+
+        $index = array_search('stock', $GLOBALS['TL_DCA']['tl_iso_product']['list']['label']['fields'], true);
+
+        $product     = Product::findByPk($row['id']);
+        $productType = $product->getRelated('type');
+        if (null === $productType || !$productType->stockmanagement_active) {
+            return $args;
+        }
+
+        if ($index !== false) {
+            $stock = Stock::getStockForProduct($row['id']);
+
+            $args[$index] = (false !== $stock) ? $stock : 'N/A';
+        }
+
+        return $args;
+    }
+}
